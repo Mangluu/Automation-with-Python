86d845db8b48b2a207893bf422cf0fee1ff62265
Manglu <shivangzephyr@gmail.com>
2020-05-25 22:45:24 +0530

    25_05

---
+++ Interacting with OS/directory.py
@@ -0,0 +1,7 @@
+import os
+# os.remove("novel.txt")
+print(os.path.exists("novel.txt"))
+# print(os.path.getmtime(("novel.txt")))
+print(os.getcwd())
+print(os.mkdir("new"))
+print(os.listdir())
---
+++ Interacting with OS/files.py
@@ -0,0 +1,2 @@
+with open("novel.txt", "a") as file:
+    file.write("This is line two")
